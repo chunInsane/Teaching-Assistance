b3a7c73375a18a4ea7acefa1c357bc355ac18643
chuninsane <chuninsane@163.com>
2016-05-25 18:07:48 +0800

    Revert "management page"
    This reverts commit f8d2a060ac73ee4f3160cfbaf2d63d60889adf91.

--- src/main/webapp/fis-conf.js
+++ src/main/webapp/fis-conf.js
@@ -5,27 +5,12 @@
 //fis.config.set('roadmap.ext.scss', 'css');
 
 
-// fis.config.merge({
-// 	roadmap : {
-// 		ext : {
-// 			less : 'css',
-// 			scss:'css',
-// 			md:'html',
-// 		}
-// 	}
-// });
-
-fis.config.set('livereload.port', 8133);
-
-fis.config.set('modules.parser.sass', 'node-sass');
-fis.config.set('modules.parser.scss', 'node-sass');
-
-fis.config.set('roadmap.ext.sass', 'css');
-fis.config.set('roadmap.ext.scss', 'css');
-
-fis.config.set('roadmap.path',[
-    {
-        reg:/^\/resources\/scss\/(.*)/i,
-        release:'/resources/css/$1'
-    },
-]);
+fis.config.merge({
+	roadmap : {
+		ext : {
+			less : 'css',
+			scss:'css',
+			md:'html',
+		}
+	}
+});
